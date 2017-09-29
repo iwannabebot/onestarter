aaca688054748a6a52860369be3f8700721a3a9f
Nilay.Vishwakarma <Nilay.Vishwakarma@grapecity.com>
2017-09-29 20:19:39 +0530

    init

---
+++ typings/express-flash.d.ts
@@ -0,0 +1,13 @@
+
+/// <reference types="express" />
+
+// add RequestValidation Interface on to Express's Request Interface.
+declare namespace Express {
+    interface Request extends Flash {}
+}
+
+interface Flash {
+    flash(type: string, message: any): void;
+}
+
+declare module "express-flash";
